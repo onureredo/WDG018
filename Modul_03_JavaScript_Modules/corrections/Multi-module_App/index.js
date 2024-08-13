3aae2bda8e45f24cbb9fba36ffb26f953b96cd2a
stephan ullmann <stephan.ullmann@posteo.de>
2024-08-13 17:35:55 +0200

    typos

--- Modul_03_JavaScript_Modules/corrections/Multi-module_App/index.js
+++ Modul_03_JavaScript_Modules/corrections/Multi-module_App/index.js
@@ -2,7 +2,7 @@ import { getProducts } from './modules/network.js';
 import { albertsFn, displayProducts } from './modules/ui.js';
 import { handleProductsClick } from './modules/storage.js';
 
-console.log('Läuft');
+// console.log('Läuft');
 
 window.addEventListener('load', async () => {
   const products = await getProducts(); // getProduct() gibt ein Promise zurück, also müssen wir warten, bis es aufgelöst wird
@@ -13,9 +13,9 @@ window.addEventListener('load', async () => {
   // albertsFn();
   handleProductsClick();
 
-  // hier lesen wir die Dinge aus dem localStorage doppel. Einmal muss der gesamte String in ein Array umgewandelt werden.
-  // Wenn er Objekte enthält, dann parsen wir diese nocheinmal extra, denn wir mussten sie vor dem speichern selbst in Strings umwandeln.
-  // Das ? ist sog. 'optional chaining' - nut wenn wir etwas im localStorage haben, dann versuchen wir über die Elemente zu loopen.
+  // Hier lesen wir die Dinge aus dem localStorage doppel. Einmal muss der gesamte String in ein Array umgewandelt werden.
+  // Wenn er Objekte enthält, dann parsen wir diese noch einmal extra, denn wir mussten sie vor dem Speichern selbst in Strings umwandeln.
+  // Das ? ist sog. 'optional chaining' - nur wenn wir etwas im localStorage haben, dann versuchen wir über die Elemente zu loopen.
   const storedItems = JSON.parse(localStorage.getItem('cart'))?.map((item) => JSON.parse(item));
   console.log(storedItems);
 });
