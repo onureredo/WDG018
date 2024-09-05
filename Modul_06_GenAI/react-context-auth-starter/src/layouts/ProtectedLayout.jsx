4dd63652195b59ea4baf94c675b7417ea78594ce
stephan ullmann <stephan.ullmann@posteo.de>
2024-09-05 11:53:16 +0200

    Nachbesserung ProtectedLayout

--- Modul_06_GenAI/react-context-auth-starter/src/layouts/ProtectedLayout.jsx
+++ Modul_06_GenAI/react-context-auth-starter/src/layouts/ProtectedLayout.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
 
 const ProtectedLayout = () => {
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) navigate('/login');
-  }, [navigate]);
+    // const token = localStorage.getItem('token');
+    // if (!token) navigate('/login');
+    if (!user) navigate('/login');
+  }, [navigate, user]);
 
   return <Outlet />;
 };
